e28be635875d7ff42caf0b44fe938459e6e00e74
Gael <gael@shippabo.com>
2020-07-19 15:01:00 +0200

    Add ts and remove example code

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home({ allPostsData }) {
+  return (
+    <>
+      <Head>
+        <title>ManuCam</title>
+      </Head>
+      <div>Hello World!</div>
+    </>
+  );
+}
