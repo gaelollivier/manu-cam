096142c6be81beb82d884cf01562305f11b45323
Gael <gael@shippabo.com>
2021-06-28 10:40:08 -0700

    Update dataset tools scripts

--- tools/build-dataset.js
+++ tools/build-dataset.js
@@ -7,9 +7,44 @@ const fs = require('fs');
     })
   );
 
-  console.log(
-    images
-      .filter(({ annotations }) => annotations?.boundingBoxes?.length > 0)
-      .map(({ annotations }) => annotations.boundingBoxes[0])
+  const annotatedImages = images.filter(
+    ({ annotations }) => annotations?.boundingBoxes?.length > 0
   );
+
+  console.log('Annotated images count:', annotatedImages.length);
+
+  const annotations = annotatedImages
+    .map((image) =>
+      image.annotations.boundingBoxes.map((annotation) => ({
+        image,
+        annotation,
+      }))
+    )
+    .flat();
+
+  console.log('Annotations count:', annotations.length);
+
+  const annotationsCSV = annotations
+    .map(({ image, annotation }) => {
+      return [
+        'UNASSIGNED',
+        `gs://manu-cam-training-data/images/${image._id}.jpg`,
+        // Label
+        'turtle',
+        // NOTE: gcloud expects normalized coordinates
+        Math.min(annotation.x1, 1).toFixed(5),
+        Math.min(annotation.y1, 1).toFixed(5),
+        '',
+        '',
+        Math.min(annotation.x2, 1).toFixed(5),
+        Math.min(annotation.y2, 1).toFixed(5),
+        '',
+        '',
+      ].join(',');
+    })
+    .join('\n');
+
+  fs.writeFileSync(`${__dirname}/data/labels.csv`, annotationsCSV, {
+    encoding: 'utf-8',
+  });
 })();
--- tools/get-images.js
+++ tools/get-images.js
@@ -3,27 +3,30 @@ const sharp = require('sharp');
 const request = require('request');
 
 (async () => {
-  const input = JSON.parse(
-    fs.readFileSync(`${__dirname}/annotations.json`, {
+  const { images } = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/images-export.json`, {
       encoding: 'utf-8',
     })
   );
 
-  for (const { image } of input.filter(({ turtle }) => turtle)) {
-    const filename = `${__dirname}/images/${image.time
-      .substr(0, 16)
-      .replace('T', '_')
-      .replace(/:/g, '-')}.jpg`;
+  const annotatedImages = images.filter(
+    ({ annotations }) => annotations?.hasManu
+  );
+
+  let index = 0;
+
+  for (const image of annotatedImages) {
+    const filename = `${__dirname}/data/images/${image._id}.jpg`;
 
     await new Promise((resolve) =>
       request
         .get(image.files.large.mediaLink)
         // Resize to fix google cloud max image dimensions
-        .pipe(sharp().resize({ height: 1080 }))
+        .pipe(sharp().resize({ width: 1024, height: 1024, fit: 'inside' }))
         .pipe(fs.createWriteStream(filename))
         .on('close', resolve)
     );
 
-    console.log(filename);
+    console.log(`${++index}/${annotatedImages.length}`);
   }
 })();
